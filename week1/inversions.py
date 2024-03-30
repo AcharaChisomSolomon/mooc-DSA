28840b1086ee9145580f8e8c065016d9ce4f0003
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-03-30 12:38:35 +0000

    inversions optimised

--- week1/inversions.py
+++ week1/inversions.py
@@ -3,7 +3,7 @@ def count(nums: list):
 
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
-            if nums[i] > nums[j] and i < j:
+            if nums[i] > nums[j]:
                 total += 1
 
     return total
