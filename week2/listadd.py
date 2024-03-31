b6568caef3e0703c6a2bb037e5ee3662020543a4
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-03-31 16:43:19 +0100

    listadd implemented

---
+++ week2/listadd.py
@@ -0,0 +1,8 @@
+def create(t: list, x):
+    return [num + x for num in t]
+
+
+if __name__ == "__main__":
+    print(create([1,2,3],1)) # [2,3,4]
+    print(create([1,1,1,1,1],4)) # [5,5,5,5,5]
+    print(create([0],10**9)) # [1000000000]
