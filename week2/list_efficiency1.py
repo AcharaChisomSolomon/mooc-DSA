3d35cb622434bb3e94b9037238bc73a9945102d9
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-03-31 16:35:48 +0100

    efficiency for list adding and deletions learnt

---
+++ week2/list_efficiency1.py
@@ -0,0 +1,21 @@
+import time
+
+n = 10 ** 5
+print("n:", n)
+numbers = []
+
+start_time1 = time.time()
+for i in range(n):
+    numbers.append(i + 1)
+end_time1 = time.time()
+
+print("additions:")
+print("time:", round(end_time1 - start_time1, 2), "s")
+
+start_time2 = time.time()
+for i in range(n):
+    numbers.pop()
+end_time2 = time.time()
+
+print("deletions:")
+print("time:", round(end_time2 - start_time2, 2), "s")
